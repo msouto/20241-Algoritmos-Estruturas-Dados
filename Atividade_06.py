e72d84a8e2b3944c2e3db2121a84deb6d0283085
Moisés Souto <msouto@users.noreply.github.com>
2024-09-06 17:16:17 -0300

    Update Atividade_06.py

--- Atividade_06.py
+++ Atividade_06.py
@@ -4,16 +4,24 @@
 #Descrição: Crie uma função fatorial(n) que receba um número inteiro n e retorne o
 #fatorial desse número. O fatorial de n é o produto de todos os números inteiros de 1 até
 #n.
+def fatorial(n):
+  pass
 
 #Questão 2 - Vetor: Soma de Elementos Pares
 #Descrição: Crie uma função soma_pares(vetor) que receba um vetor de inteiros e
 #retorne a soma de todos os elementos pares do vetor.
+def soma_pares(vetor):
+  pass
 
 #Questão 3 - Input/Output: Conversor de Temperatura
 #Descrição: Crie uma função conversor_temperatura() que leia uma temperatura em
 #graus Celsius fornecida pelo usuário, converta para Fahrenheit e imprima o resultado. A
 #fórmula de conversão é: F = C * 9/5 + 32.
+def conversor_temperatura():
+  pass
 
 #Questão 4 - Matriz Bidimensional: Soma de Matrizes
 #Descrição: Crie uma função soma_matrizes(m1, m2) que receba duas matrizes 2x2 e
 #retorne uma nova matriz, que seja a soma das duas matrizes recebidas.
+def soma_matrizes(m1,m2):
+  pass
