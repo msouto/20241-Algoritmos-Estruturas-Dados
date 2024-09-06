977dc7e89537c94fd239eb68f44d91aab9d77e15
Moisés Souto <msouto@users.noreply.github.com>
2024-09-06 17:05:00 -0300

    Create Atividade_06.py

---
+++ Atividade_06.py
@@ -0,0 +1,12 @@
+#Atividade 06 - Exercicíos II
+
+#Questão 1 - Função: Cálculo do Fatorial
+#Descrição: Crie uma função fatorial(n) que receba um número inteiro n e retorne o
+#fatorial desse número. O fatorial de n é o produto de todos os números inteiros de 1 até
+#n.
+
+#Questão 2
+
+#Questão 3
+
+#Questão 4
